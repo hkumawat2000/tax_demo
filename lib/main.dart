daeb5349a0bfe817da94cac81587d2d11ee03dd7
himanshukumawat <himanshu.kumawat@atriina.com>
2023-08-09 15:29:01 +0530

    Set theme to light

--- lib/main.dart
+++ lib/main.dart
@@ -27,6 +27,7 @@ class MyApp extends StatelessWidget {
       title: 'Taxation',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        brightness: Brightness.light
       ),
       home: const AllCalculator(),
     );
